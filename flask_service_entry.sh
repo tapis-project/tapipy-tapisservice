9fa56345900e941266e87945c082273b245bb87c
Joe Stubbs <joestubbs5@gmail.com>
2022-01-10 13:19:53 -0600

    fix bash typos

--- flask_service_entry.sh
+++ flask_service_entry.sh
@@ -3,6 +3,8 @@
 export PYTHONPATH=$PYTHONPATH:/home/tapis
 cd /home/tapis/service; /usr/local/bin/gunicorn -k $workerCls -w $processes --threads $threads -b :5000 api:app
 
-if [$DEBUG_SLEEP_LOOP == "sleep"]; then
+
+
+if [ "$DEBUG_SLEEP_LOOP" == "sleep" ]; then
   while true; do sleep 86400; done
 fi
